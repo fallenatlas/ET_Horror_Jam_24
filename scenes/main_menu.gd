cd5b2318e368a7541d04e33ae7eb05d1fdc1ccb2
fallenatlas <tiago.a.p.antunes@gmail.com>
2024-03-27 18:08:43 +0000

    add more assets and reorganize files

--- scenes/main_menu.gd
+++
@@ -1,9 +0,0 @@
-extends Control
-
-
-func _on_start_pressed():
-	get_tree().change_scene_to_file("res://scenes/street.tscn")
-
-
-func _on_credit_pressed():
-	get_tree().change_scene_to_file("res://scenes/credits.tscn")
