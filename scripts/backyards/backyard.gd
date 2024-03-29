5e1a5cd90030a9150baca96d5cfe9647540dc1ca
Sebastiao Carvalho <sebastiaovscarvalho@tecnico.ulisboa.pt>
2024-03-29 15:28:38 +0000

    add backyard entrance dialogue

--- scripts/backyards/backyard.gd
+++ scripts/backyards/backyard.gd
@@ -1,9 +1,14 @@
 extends Node2D
 
+@export var info : Backyard_Resource
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	#play dialog if first time
+	if info.first_time:
+		var dialog_name = info.owner + "House"
+		Dialogic.start(dialog_name)
+		info.first_time = false
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
