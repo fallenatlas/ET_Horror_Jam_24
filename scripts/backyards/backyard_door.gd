de7c77138fbc860d5f464e6c419e07c07475684b
Sebastiao Carvalho <sebastiaovscarvalho@tecnico.ulisboa.pt>
2024-03-29 12:27:03 +0000

    add scene navigation

--- scripts/backyards/backyard_door.gd
+++ scripts/backyards/backyard_door.gd
@@ -6,4 +6,5 @@ extends Node
 
 func _on_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton && event.pressed:
-		var dial
+		var scene_name = "res://scenes/backyards/" + info.owner + "_backyard.tscn"
+		get_tree().change_scene_to_file(scene_name)
