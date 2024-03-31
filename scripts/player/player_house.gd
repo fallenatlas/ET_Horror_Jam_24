25a7d517bb609028c07295f37c0afb07a4cda988
Sebastiao Carvalho <sebastiaovscarvalho@tecnico.ulisboa.pt>
2024-03-31 23:58:21 +0100

    update stuff on player placard and add some more timelines

--- scripts/player/player_house.gd
+++ scripts/player/player_house.gd
@@ -9,25 +9,22 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	print_debug(game_state.day_or_night + str(game_state.current_day))
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
+	Dialogic.signal_event.connect(_on_signal_event)
+	
+func _on_signal_event(argument : String):
+	match argument:
+		"changeDay":
+			_on_exit_button_pressed()
 
 func _on_call_button_pressed():
 	if characters.right_choices() and objects.right_choices():
-		print("well done")
+		Dialogic.start("goodAccusation")
 	elif not characters.enough_choices():
-		print("choose a suspect")
+		Dialogic.start("needSuspect")
 	elif not objects.enough_choices():
-		print("not enouth evidence")
+		Dialogic.start("needEvidence")
 	else:
-		print("rip")
+		Dialogic.start("falseAccusation")
 
 func _on_exit_button_pressed():
-	pass # Replace with function body.
-	#go to next day/night...
-	#probably change something, maybe game_manager.next_scene()
 	get_tree().change_scene_to_file("res://scenes/player/player_house_view.tscn")
